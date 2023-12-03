2eb09ed75cfe17e3870d4beedb2c94b31ed827b1
Abdurakhmon <abdurahmonavezdjanov031@gmail.com>
2023-12-03 13:00:14 +0500

    Initialize the project

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+
+import { Button } from '@/components/ui/button'
+
+
+const Home = () => {
+
+  return (
+    <div>Home
+      <Button  >
+        Pay
+      </Button>
+    </div>
+  )
+}
+
+export default Home
